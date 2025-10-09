450c9910146a1c9de060114b8f083ba2d5566fcb
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2025-10-09 20:59:22 +0200

    functional mvp
    TODO:
    - code clean-up
    - create custom widgets instead of using a single file
    - find best practice for state-handling
    - find general best practice for dart and flutter
    - styling, LOTS of styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'US imperial to metric converter',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,15 +31,15 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -47,31 +50,65 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController.addListener(tryConversion);
+  }
+  final TextEditingController _textEditingController = TextEditingController();
+  final List<String> _usMeasurements = ['Fahrenheit', 'Cups', 'Miles'];
+  final List<String> _euMeasurements = ['Celsius', 'Deciliters', 'Kilometers'];
 
-  void _incrementCounter() {
+  String? _selectedFrom = 'Fahrenheit';
+  String? _selectedTo = 'Deciliters';
+
+  double _lastConversion = 0;
+
+
+  void _selectedConversionType(bool isFrom, String selectedValue) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if(isFrom) {
+        _selectedFrom = selectedValue;
+      } else {
+        _selectedTo = selectedValue;
+      }
+    });
+
+    tryConversion();
+  }
+  void tryConversion(){
+    if(_selectedFrom == 'Fahrenheit' && _selectedTo == 'Celsius') fahrenheitToCelsius(double.parse(_textEditingController.text));
+
+    if(_selectedFrom == 'Miles' && _selectedTo == 'Kilometers') milesToKilometers(double.parse(_textEditingController.text));
+
+    if(_selectedFrom == 'Cups' && _selectedTo == 'Deciliters') cupsToDeciliters(double.parse(_textEditingController.text));
+  }
+  void fahrenheitToCelsius(double toConvert) {
+    setState(() {
+      _lastConversion = (toConvert - 32) / 1.8;
+    });
+  }
+  void milesToKilometers(double toConvert) {
+    setState(() {
+      _lastConversion = toConvert * 1.609344;
+    });
+  }
+  void cupsToDeciliters(double toConvert) {
+    setState(() {
+      _lastConversion = toConvert * 2.365882365;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
+
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
@@ -83,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('US imperial to metric converter'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -103,20 +140,80 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            // FROM -> TO SELECTOR
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    DropdownButton(
+                      value: _selectedFrom,
+                      items:
+                        _usMeasurements.map((item) => DropdownMenuItem(
+                          value: item,
+                          child: Text(item),
+                          ),
+                        ).toList(),
+                      onChanged: (item) => _selectedConversionType(true, item!),
+                    ),
+                    Text('from'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    DropdownButton(
+                      value: _selectedTo,
+                      items:
+                      _euMeasurements.map((item) => DropdownMenuItem(
+                        value: item,
+                        child: Text(item),
+                      ),
+                      ).toList(),
+                      onChanged: (item) => _selectedConversionType(false, item!),
+                    ),
+                    Text('to'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _textEditingController,
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter value to convert',
+                        floatingLabelAlignment: FloatingLabelAlignment.center
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      '${_textEditingController.text} $_selectedFrom = ${_lastConversion.toStringAsFixed(2)} $_selectedTo',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
